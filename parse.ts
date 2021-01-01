4a2a670f2014a7e79bd5ca7062bd532a2b932618
Benjamin Brandmeier <benjamin@brandmeier.email>
2021-01-01 20:45:43 +0100

    feature: create dependency graph for UI selected singular files only

--- parse.ts
+++ parse.ts
@@ -1,36 +1,37 @@
-function createVisualizationData(): void {
-    const allFilesToBeParsed = Deno.readDirSync('files');
-    let mapOfRoutes = new Map();
-    [...allFilesToBeParsed].filter(f => f.name !== '.gitignore').forEach(f => {
-        const fileAsText = readInput(f.name);
-        const qualifier = f.name.substr(0, f.name.indexOf('.'));
-        console.log('Parsing', f.name);
-        const mapOfStaticImports = buildStaticImportsMap(fileAsText);
-        const allRouteDefinitionsAsText = getAllRouteDefinitions(fileAsText);
-        mapOfRoutes = buildRouteMap(allRouteDefinitionsAsText, mapOfStaticImports, mapOfRoutes, qualifier);
-    });
-    const visualizationData = buildElements(mapOfRoutes);
-
-    writeDataToFile(visualizationData);
-    console.log('Finished.');
-}
-
 const readInput = (fileName: string) => Deno.readTextFileSync('files/' + fileName);
 const getAllRouteDefinitions = (code: string) => code.match(/from(.|\n|\r)*?;/g)!;
 const shortenQualifier = (s: string) => s.split('').filter(c => [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ.'].includes(c)).join('');
 const removeAllQuotes = (routeDefiniton: string) => routeDefiniton.replace('"', '');
 const isQualifiedAlready = (to: string) => to.includes('.');
+const formatDataToBeValidJson = (visualizationData: string) => '[' + visualizationData.slice(0, -1) + ']';
+const writeDataToFile = (visualizationData: string) => Deno.writeTextFileSync('src/assets/data.json', visualizationData);
 
-function fullQualifyTo(to: string, mapOfStaticImports: Map<string, string>, qualifier: string): string {
-    if (isQualifiedAlready(to)) {
-        return shortenQualifier(to.split('.')[0]) + '.' + to.split('.')[1];
-    } else if (mapOfStaticImports.has(to)) {
-        return shortenQualifier(mapOfStaticImports.get(to)!) + '.' + to;
-    } else if (to.includes('cxf:bean') || to.includes('cxfrs:bean')) {
-        return to.includes('?') ? to.substring(1, to.indexOf('?')) : to;
-    } else {
-        return shortenQualifier(qualifier + '.').concat(to);
-    }
+function generateVisualizationDataForAllProvidedFiles(): void {
+    const allFilesToBeParsed = Deno.readDirSync('files');
+    const visualizationData = [...allFilesToBeParsed]
+        .filter(f => f.name !== '.gitignore')
+        .map(buildVisualizationDataForSingleFile)
+        .join('\n');
+
+    writeDataToFile(formatDataToBeValidJson(visualizationData));
+    console.log('Finished.');
+}
+
+function buildVisualizationDataForSingleFile(file: Deno.DirEntry): string {
+    console.log('Parsing', file.name);
+    const fileAsText = readInput(file.name);
+    const qualifier = file.name.substr(0, file.name.indexOf('.'));
+    const mapOfStaticImports = buildStaticImportsMap(fileAsText);
+    const allRouteDefinitionsAsText = getAllRouteDefinitions(fileAsText);
+    const mapOfRoutes = buildRouteMap(allRouteDefinitionsAsText, mapOfStaticImports, new Map(), qualifier);
+    return buildCytoscapeElements(mapOfRoutes, file.name);
+}
+
+function buildStaticImportsMap(code: string): Map<string, string> {
+    const mapOfStaticImports = new Map();
+    const matchesFoundIterator = code.matchAll(/import static .*\.(.*)\.(.*);/g);
+    [...matchesFoundIterator].forEach(m => mapOfStaticImports.set(m[2], m[1]));
+    return mapOfStaticImports;
 }
 
 function buildRouteMap(allRouteDefinitions: string[], mapOfStaticImports: Map<string, string>, mapOfRoutes: Map<string, any>, qualifier: string): Map<string, string[]> {
@@ -48,25 +49,26 @@ function buildRouteMap(allRouteDefinitions: string[], mapOfStaticImports: Map<st
     return mapOfRoutes;
 }
 
-function buildStaticImportsMap(code: string): Map<string, string> {
-    const mapOfStaticImports = new Map();
-    const matchesFoundIterator = code.matchAll(/import static .*\.(.*)\.(.*);/g);
-    [...matchesFoundIterator].forEach(m => mapOfStaticImports.set(m[2], m[1]));
-    return mapOfStaticImports;
+function fullQualifyTo(to: string, mapOfStaticImports: Map<string, string>, qualifier: string): string {
+    if (isQualifiedAlready(to)) {
+        return shortenQualifier(to.split('.')[0]) + '.' + to.split('.')[1];
+    } else if (mapOfStaticImports.has(to)) {
+        return shortenQualifier(mapOfStaticImports.get(to)!) + '.' + to;
+    } else if (to.includes('cxf:bean') || to.includes('cxfrs:bean')) {
+        return to.includes('?') ? to.substring(1, to.indexOf('?')) : to;
+    } else {
+        return shortenQualifier(qualifier + '.').concat(to);
+    }
 }
 
-function buildElements(mapOfRoutes: Map<string, string[]>): string {
-    let vizualizationData = '';
-
-    [...mapOfRoutes.keys()].forEach(r => vizualizationData += '{"data": { "id": "' + r + '" }},\n');
+function buildCytoscapeElements(mapOfRoutes: Map<string, string[]>, file: string): string {
+    let visualizationData = '\n' + [...mapOfRoutes.keys()].map(r => '{"data": { "id": "' + r + '", "file": "' + file + '" }},').join('\n');
     mapOfRoutes.forEach((tos, from) => {
         tos?.forEach(to => {
-            vizualizationData += '\n{"data": { "id": "' + Math.random() + '", "source": "' + from + '", "target": "' + to + '" }},'; // might also create object and convert to json
+            visualizationData += '\n{"data": { "id": "' + Math.random() + '", "source": "' + from + '", "target": "' + to + '", "file": "' + file + '" }},';
         });
     });
-    return vizualizationData.slice(0, vizualizationData.length - 1);
+    return visualizationData;
 }
 
-const writeDataToFile = (vizualizationData: string) => Deno.writeTextFileSync('src/assets/data.json', '[' + vizualizationData + ']');
-
-createVisualizationData();
+generateVisualizationDataForAllProvidedFiles();
