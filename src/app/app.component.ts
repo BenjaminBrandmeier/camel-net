7c7f3776116263fcab735a2aa6e6df78584bd935
Benjamin Brandmeier <bdvlop@gmail.com>
2021-01-01 20:45:43 +0100

    feature: create dependency graph for UI selected singular files only

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import {edgeStyle, nodeStyle, StylingService} from './styling.service';
 
 const isNodeSelected = (target: any, cy: Core) => target !== cy && !target.isEdge();
 const findNode = (route: string) => e => e.data().id.toUpperCase().includes(route.toUpperCase());
+const getAllFileNames = (elements: ElementDefinition[]) => [...new Set(elements.map(e => e.data.file))].sort().concat('All files');
+const isElementPartOfAppliedFilter = (element: ElementDefinition, filter: string) => filter === 'All files' ? true : element.data.file === filter;
 
 @Component({
     selector: 'app-root',
@@ -13,38 +15,34 @@ const findNode = (route: string) => e => e.data().id.toUpperCase().includes(rout
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
+    allFilenamesContainingRoutes = [];
+    private cy: Core;
+    private filter = '';
+    private data;
+
     constructor(private readonly stylingService: StylingService,
     ) {
     }
-
-    private cy: Core;
 
     ngOnInit(): void {
         cytoscape.use(cise);
 
         fetch('assets/data.json')
             .then(response => response.json())
-            .then((data: ElementDefinition[]) => this.initializeCytoscape(data))
+            .then((data: ElementDefinition[]) => {
+                this.data = data;
+                this.allFilenamesContainingRoutes = getAllFileNames(data);
+                this.filter = this.allFilenamesContainingRoutes[0];
+            })
+            .then(() => this.initializeCytoscape())
             .catch((r) => console.error(r, 'Initialization of camel-net failed. Please follow the guide on https://github.com/BenjaminBrandmeier/camel-net'));
     }
 
-    onSearchRouteClick(route: string): boolean {
-        const searchResult = this.allElements().filter(findNode(route));
-
-        if (searchResult.length > 1) {
-            alert('Found more than one:\n' + searchResult.map(e => e.data().id).join('\n'));
-        } else if (searchResult.length === 0) {
-            alert(`Route ${route} not found`);
-        } else {
-            this.stylingService.colorDependencyPath(searchResult[0], this.allElements());
-        }
-        return false; // return false to suppress page refresh
-    }
-
-    private initializeCytoscape(data: ElementDefinition[]): void {
+    private initializeCytoscape(): void {
         this.cy = cytoscape({
             container: document.getElementById('cy'),
-            elements: data,
+            elements: this.data.filter(e => isElementPartOfAppliedFilter(e, this.filter)),
             style: [nodeStyle, edgeStyle],
             layout: ciseLayout,
         });
@@ -60,6 +58,24 @@ export class AppComponent implements OnInit {
                 this.stylingService.colorDependencyPath(target, this.allElements());
             }
         };
+    }
+
+    onSearchRouteClick(route: string): boolean {
+        const searchResult = this.allElements().filter(findNode(route));
+
+        if (searchResult.length > 1) {
+            alert('Found more than one:\n' + searchResult.map(e => e.data().id).join('\n'));
+        } else if (searchResult.length === 0) {
+            alert(`Route ${route} not found`);
+        } else {
+            this.stylingService.colorDependencyPath(searchResult[0], this.allElements());
+        }
+        return false; // return false to suppress page refresh
+    }
+
+    onFilterChange(value: string): void {
+        this.filter = value;
+        this.initializeCytoscape();
     }
 
     private allElements = (): CollectionReturnValue => this.cy.elements();
