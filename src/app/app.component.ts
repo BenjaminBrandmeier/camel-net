3ff87c96abd77f576ac044f34c1621420664855c
Benjamin Brandmeier <benjamin@brandmeier.email>
2021-01-03 22:15:42 +0100

    make route search global and display found dependency graph solely

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,13 @@ import cise from 'cytoscape-cise';
 import {LayoutService} from './layout/layout.service';
 import {edgeStyle, nodeStyle, StylingService} from './styling/styling.service';
 
+const ALL_FILES = 'All files';
 const isNodeSelected = (target: any, cy: Core) => target !== cy && !target.isEdge();
-const findNode = (route: string) => e => e.data().id.toUpperCase().includes(route.toUpperCase());
-const getAllFileNames = (elements: ElementDefinition[]) => [...new Set(elements.map((e) => e.data.file))].sort().concat('All files');
-const isElementPartOfAppliedFilter = (element: ElementDefinition, filter: string) => filter === 'All files' ? true : element.data.file === filter;
+const findNodeLazy = (route: string) => e => e.data().id.toUpperCase().includes(route.toUpperCase());
+const findDataNodeStrict = (route: string) => e => e.data.id.toUpperCase() === route.toUpperCase();
+const findDataNodeLazy = (route: string) => e => e.data.id.toUpperCase().includes(route.toUpperCase());
+const getAllFileNames = (elements: ElementDefinition[]) => [...new Set(elements.map((e) => e.data.file))].sort().concat(ALL_FILES);
+const isElementPartOfAppliedFilter = (element: ElementDefinition, filter: string) => filter === ALL_FILES ? true : element.data.file === filter;
 const isActualNode = e => !e.data.hasOwnProperty('source') && !e.data.hasOwnProperty('target');
 const getAllFilteredElementDefinitions = (data, filter: string) => data.filter(e => isElementPartOfAppliedFilter(e, filter));
 const getAllFilteredNodes = (data, filter) => getAllFilteredElementDefinitions(data, filter).filter(isActualNode);
@@ -20,11 +23,12 @@ const getAllFilteredNodes = (data, filter) => getAllFilteredElementDefinitions(d
 export class AppComponent implements OnInit {
 
     allFilenamesContainingRoutes = [];
-    isClusteredViaPackages = false;
+    fileFilter = undefined;
+    searchText = '';
 
     private cy: Core;
-    private filter = '';
     private data: ElementDefinition[];
+    private originalData;
     private focusedNode = null;
 
     constructor(private readonly stylingService: StylingService,
@@ -39,23 +43,27 @@ export class AppComponent implements OnInit {
             .then(response => response.json())
             .then((data: ElementDefinition[]) => {
                 this.data = data;
+                this.originalData = data;
                 this.allFilenamesContainingRoutes = getAllFileNames(data);
-                this.filter = this.allFilenamesContainingRoutes[0];
+                this.fileFilter = this.fileFilter ?? this.allFilenamesContainingRoutes[0];
             })
             .then(() => this.initializeCytoscape())
             .catch((r) => console.error(r, '\n\nInitialization of camel-net failed. \nPlease follow the guide on \n\nhttps://github.com/BenjaminBrandmeier/camel-net'));
     }
 
-    private initializeCytoscape(): void {
+    private initializeCytoscape(useFancyLayout = true): void {
         this.cy = cytoscape({
             container: document.getElementById('cy'),
-            elements: getAllFilteredElementDefinitions(this.data, this.filter),
+            elements: getAllFilteredElementDefinitions(this.data, this.fileFilter),
             style: [nodeStyle, edgeStyle],
-            layout: this.layoutService.getDefaultLayout(getAllFilteredNodes(this.data, this.filter), this.isClusteredViaPackages),
+            layout: useFancyLayout ? this.getFancyLayout() : 'grid',
         });
         this.cy.bind('click', this.onGraphElementClick(this.cy));
         this.cy.bind('tap', this.onGraphElementClick(this.cy));
     }
+
+    private getFancyLayout = () => this.layoutService.getDefaultLayout(getAllFilteredNodes(this.data, this.fileFilter), this.isPackageClusteringActive());
+    private isPackageClusteringActive = (): boolean => this.fileFilter === ALL_FILES && this.searchText === '';
 
     private onGraphElementClick(cy: Core): (event) => void {
         return (event) => {
@@ -69,35 +77,53 @@ export class AppComponent implements OnInit {
         };
     }
 
-    onSearchRouteClick(route: string): boolean {
-        const searchResult = this.allElements().filter(findNode(route));
+    onSearchRouteClick(): boolean {
+        let searchResult = this.originalData.filter(findDataNodeStrict(this.searchText));
+        searchResult = searchResult.length === 0 ? this.originalData.filter(findDataNodeLazy(this.searchText)) : searchResult;
+        const uniqueResults = [...new Set(searchResult.map(s => s.data.id))];
 
-        if (searchResult.length > 1) {
-            alert('Found more than one:\n' + searchResult.map(e => e.data().id).join('\n'));
-        } else if (searchResult.length === 0) {
-            alert(`Route ${route} not found`);
+        if (uniqueResults.length > 1) {
+            alert('Found more than one:\n' + uniqueResults.join('\n'));
+        } else if (uniqueResults.length === 0) {
+            alert(`Route ${this.searchText} not found`);
         } else {
-            this.stylingService.colorDependencyPath(searchResult[0], this.allElements());
+            this.preRender();
+            this.renderFoundDependencyGraphOnly();
         }
         return false; // return false to suppress page refresh
     }
 
-    onFilterChange(value: string): void {
-        this.filter = value;
-        this.initializeCytoscape();
+    private preRender(): void {
+        this.fileFilter = ALL_FILES;
+        this.data = this.originalData;
+        this.initializeCytoscape(false);
     }
 
-    changePackageCluster(): void {
-        this.isClusteredViaPackages = !this.isClusteredViaPackages;
+    private renderFoundDependencyGraphOnly(): void {
+        const foundNode: any = this.allElements().filter(findNodeLazy(this.searchText));
+        const entirePathOfFoundNode = [...foundNode.predecessors(), ...foundNode.successors(), foundNode].map(d => d.data());
+        this.data = this.originalData.filter(e => entirePathOfFoundNode.includes(e.data));
         this.initializeCytoscape();
+        this.stylingService.colorDependencyPath(this.allElements().filter(findNodeLazy(this.searchText)), this.allElements());
+    }
+
+    onFilterChange(value: string): void {
+        this.searchText = '';
+        this.fileFilter = value;
+        this.ngOnInit();
     }
 
     private allElements = (): CollectionReturnValue => this.cy.elements();
 
     @HostListener('window:keydown', ['$event'])
     onKeydown($event: KeyboardEvent): void {
-        if ($event?.key === 'Delete') {
-            this.focusedNode?.remove();
+        switch ($event?.key) {
+            case 'Delete':
+                this.focusedNode?.remove();
+                break;
+            case 'Enter':
+                this.onSearchRouteClick();
+                break;
         }
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
